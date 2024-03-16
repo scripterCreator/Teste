c79e4e5995fd9b00913ceda3abe985897c500d47
Programmer <161971825+scripterCreator@users.noreply.github.com>
2024-03-16 05:40:01 -0300

    Update Teste.lua

--- Teste.lua
+++ Teste.lua
@@ -4,3 +4,32 @@ print(message)
 else
 print("mensagem esta em false"
 end
+
+if game.PlaceId == 1799972 then
+warn("Tu e ga#")
+else
+  print("What")
+  
+   end
+end
+
+print("porfavo entre no jogo"
+else
+print("voce entrou no jogo parabéns")
+end
+
+if Player.PlaceId == 186699 then
+local messageUser = "Voce e um corno")
+
+print(messageUser)
+
+  end
+end
+
+local Script = true
+WindowsName = "Fake Gui"
+WindowsMinizimer = Leftcontrol
+
+local button = "parent")
+_G.Script = true
+loadstring(game(Https(Github99.com/Name/User/main/Script))()
